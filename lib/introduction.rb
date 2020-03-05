31f62834022193ea6bf6fa171dff5e64ff79c204
Alice <agenkin1@gmail.com>
2020-03-05 20:37:28 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,11 +4,21 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
+#Define a method that takes and uses a parameter
+#Define a method that takes and uses two parameters
+#Define a method with an optional parameter
 
+def introduction (name)
+  puts "Hi, my name is #{name}."
+end
 
+def introduction_with_language (name, language)
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
-
-
+def introduction_with_language_optional (name, language="Ruby")
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
